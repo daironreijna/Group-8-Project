f9301da7f32cdc96023635fc5e3f7a14b4438195
Dairon Reijna <dairon.reijna@gmail.com>
2023-09-22 19:26:53 +0100

    ensure styling is consistent

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,32 +11,32 @@
 
 
 
-$("button").on("click", function(){ 
-    let landmark = $(this).attr("data-landmark")
+$("button").on("click", function () {
+  let landmark = $(this).attr("data-landmark")
 
-// alert($(this).attr("data-landmark"))
-    let wikiAPIURL =  `https://en.wikipedia.org/w/rest.php/v1/search/page?q=${landmark}&limit=1`
+  // alert($(this).attr("data-landmark"))
+  let wikiAPIURL = `https://en.wikipedia.org/w/rest.php/v1/search/page?q=${landmark}&limit=1`
 
-$("#The-cards").hide()
+  $("#The-cards").hide()
 
-$("#Home").hide()
+  $("#Home").hide()
 
-// window.location.href="index.html";
+  // window.location.href="index.html";
 
-// Performing our Fetch GET request
-fetch(wikiAPIURL)
-// After the data comes back from the API
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  // Storing an array of results in the results variable
-  let results = data.pages;
-  
-console.log(results);
+  // Performing our Fetch GET request
+  fetch(wikiAPIURL)
+    // After the data comes back from the API
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // Storing an array of results in the results variable
+      let results = data.pages;
+
+      console.log(results);
 
 
-})
+    })
 
 })
 
